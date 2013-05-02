c239ea4117937b45e28844140e0e57d18d104f11
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-05-01 22:04:09 -0500

    Exclude .gitmodules.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
 source ~/.bash_profile
