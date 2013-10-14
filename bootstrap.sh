545a9e454d838e15cb04209fc290981c24ee4910
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-10-14 12:33:23 -0500

    Init submodules on bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
+git submodule update --init
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
 source ~/.bash_profile
