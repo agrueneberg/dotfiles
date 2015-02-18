9f46e117ab0b52ae4fafd30ef30d9b5dcfa82550
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-02-18 13:46:06 -0600

    Fix typo: .pkg/.pkgs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 git submodule update --init
-mkdir -p ~/.pkg/{R,npm}
+mkdir -p ~/.pkgs/{R,npm}
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
 source ~/.bash_profile
