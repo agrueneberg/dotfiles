461bab0c61fa49903d8f4be10f8242e9970e792e
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-03-10 13:50:10 -0500

    Add git-remove-merged-branches alias.

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -2,3 +2,4 @@ alias ls='ls --color'
 alias tree='tree -C'
 alias grep='grep --color'
 alias hosts="sudo $EDITOR /etc/hosts"
+alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
