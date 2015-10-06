9c47edb51669b33288efea40c13a654298247710
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-10-06 10:00:09 -0400

    Don't show an error message if __git_ps1 can't be found.

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -5,4 +5,4 @@ elif [ -f /usr/share/git/completion/git-prompt.sh ]; then
 elif [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
   source /usr/share/git-core/contrib/completion/git-prompt.sh
 fi
-PS1='\[\e[1;31m\]\u\[\e[m\] at \[\e[1;31m\]\h\[\e[m\] in \[\e[1;31m\]\w\[\e[m\]$(__git_ps1 " on \[\e[1;31m\]%s\[\e[m\]")\n\[\e[1;31m\]$\[\e[m\] '
+PS1='\[\e[1;31m\]\u\[\e[m\] at \[\e[1;31m\]\h\[\e[m\] in \[\e[1;31m\]\w\[\e[m\]$(declare -F __git_ps1 >&/dev/null && __git_ps1 " on \[\e[1;31m\]%s\[\e[m\]")\n\[\e[1;31m\]$\[\e[m\] '
