bfc7c017a0323689a01216bb058610a83cb3907a
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-12-08 14:24:37 -0500

    Use :: instead of library to keep code short.

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -3,6 +3,6 @@ alias tree='tree -C'
 alias grep='grep --color'
 alias hosts="sudo $EDITOR /etc/hosts"
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
-alias rinstall="Rscript -e 'library(methods); library(devtools); install()'"
-alias rdocument="Rscript -e 'library(methods); library(devtools); document()'"
-alias rtidy="Rscript -e 'library(methods); library(formatR); args <- commandArgs(trailingOnly = TRUE); stopifnot(length(args) > 0); if (dir.exists(args[1])) tidy_dir(args[1], arrow = TRUE, recursive = TRUE) else tidy_source(args[1], arrow = TRUE, file = args[1])'"
+alias rinstall="Rscript -e 'library(methods); devtools::install()'"
+alias rdocument="Rscript -e 'library(methods); devtools::document()'"
+alias rtidy="Rscript -e 'library(methods); args <- commandArgs(trailingOnly = TRUE); stopifnot(length(args) > 0); if (dir.exists(args[1])) formatR::tidy_dir(args[1], arrow = TRUE, recursive = TRUE) else formatR::tidy_source(args[1], arrow = TRUE, file = args[1])'"
