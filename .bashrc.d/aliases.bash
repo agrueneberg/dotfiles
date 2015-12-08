8ed009765429aff0638b550341d1c90c0c3305f7
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-12-08 14:54:12 -0500

    Add rtest and rcheck aliases.

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -5,4 +5,6 @@ alias hosts="sudo $EDITOR /etc/hosts"
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias rinstall="Rscript -e 'library(methods); devtools::install()'"
 alias rdocument="Rscript -e 'library(methods); devtools::document()'"
+alias rtest="Rscript -e 'library(methods); devtools::test()'"
+alias rcheck="Rscript -e 'library(methods); devtools::check()'"
 alias rtidy="Rscript -e 'library(methods); args <- commandArgs(trailingOnly = TRUE); stopifnot(length(args) > 0); if (dir.exists(args[1])) formatR::tidy_dir(args[1], arrow = TRUE, recursive = TRUE) else formatR::tidy_source(args[1], arrow = TRUE, file = args[1])'"
