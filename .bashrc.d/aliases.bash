4a349342dd65434c9183a25c746b1e75ec0b3907
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-12-08 13:55:03 -0500

    Add rinstall and rdocument aliases.

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -3,3 +3,5 @@ alias tree='tree -C'
 alias grep='grep --color'
 alias hosts="sudo $EDITOR /etc/hosts"
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+alias rinstall="Rscript -e 'library(methods); library(devtools); install()'"
+alias rdocument="Rscript -e 'library(methods); library(devtools); document()'"
