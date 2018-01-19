fecf9c63df213d682b371f72ca52dceb50b859f4
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2018-01-19 08:18:43 -0500

    Remove now invalid -rubygems flag

--- .bash_profile.d/env.bash
+++ .bash_profile.d/env.bash
@@ -1,4 +1,4 @@
-export GEM_HOME=$(ruby -rubygems -e 'print Gem.user_dir')
+export GEM_HOME=$(ruby -e 'print Gem.user_dir')
 export npm_config_prefix="$HOME/.pkgs/npm"
 export GOPATH="$HOME/.pkgs/go"
 export PATH="$HOME/.local/bin:$GEM_HOME/bin:$npm_config_prefix/bin:$GOPATH/bin:$PATH"
