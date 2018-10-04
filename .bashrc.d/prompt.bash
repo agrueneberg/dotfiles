ff83caa46b8053bc7c5adf5a76f37bb0d28e2493
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2018-10-04 11:07:21 -0400

    Fix indentation

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -1,8 +1,8 @@
 if [ -f /etc/bash_completion.d/git-prompt ]; then
-  source /etc/bash_completion.d/git-prompt
+    source /etc/bash_completion.d/git-prompt
 elif [ -f /usr/share/git/completion/git-prompt.sh ]; then
-  source /usr/share/git/completion/git-prompt.sh
+    source /usr/share/git/completion/git-prompt.sh
 elif [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
-  source /usr/share/git-core/contrib/completion/git-prompt.sh
+    source /usr/share/git-core/contrib/completion/git-prompt.sh
 fi
 PS1='\[\e[1;31m\]\u\[\e[m\] at \[\e[1;31m\]\h\[\e[m\] in \[\e[1;31m\]\w\[\e[m\]$(declare -F __git_ps1 >&/dev/null && __git_ps1 " on \[\e[1;31m\]%s\[\e[m\]")\n\[\e[1;31m\]$\[\e[m\] '
