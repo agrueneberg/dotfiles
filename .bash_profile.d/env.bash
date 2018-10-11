26ec5f3f6e1614d9593d1fcf0fe33aadda7b0de8
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2018-10-11 10:59:24 -0400

    Move R library to ~/.Rlibrary

--- .bash_profile.d/env.bash
+++ .bash_profile.d/env.bash
@@ -4,4 +4,4 @@ export EDITOR='vim'
 
 export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8"
 
-[[ -z "$R_LIBS_USER" ]] && export R_LIBS_USER="$HOME/.pkgs/R"
+[[ -z "$R_LIBS_USER" ]] && export R_LIBS_USER="$HOME/.Rlibrary"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 git submodule update --init
-mkdir -p ~/.pkgs/R
+mkdir -p ~/.Rlibrary
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
 source ~/.bash_profile
 vim +'PlugInstall' +'qall' &> /dev/null
