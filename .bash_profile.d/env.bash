6f9702191a3e40021ce48b834ab1c03b38a0dcfc
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2018-10-11 10:55:45 -0400

    Remove Node support

--- .bash_profile.d/env.bash
+++ .bash_profile.d/env.bash
@@ -1,8 +1,3 @@
-if command -v node >/dev/null 2>&1; then
-    export npm_config_prefix="$HOME/.pkgs/npm"
-    export PATH="$npm_config_prefix/bin:$PATH"
-fi
-
 export PATH="$HOME/.local/bin:$PATH"
 
 export EDITOR='vim'
