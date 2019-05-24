20c23bbbf20f085474de73f5f5b07ee1ae833fb5
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2019-05-24 12:23:30 -0400

    Use --color=auto in ls and grep

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -1,7 +1,7 @@
-alias ls='ls --color'
+alias ls='ls --color=auto'
 alias ll='ls -l'
 alias tree='tree -C'
-alias grep='grep --color'
+alias grep='grep --color=auto'
 alias hosts="sudo $EDITOR /etc/hosts"
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias plink='plink --noweb'
