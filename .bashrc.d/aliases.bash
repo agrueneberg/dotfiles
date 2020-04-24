3a25e3f8b7df98ff81ba4e7990b12777cc3a6ebf
Alexander Grueneberg <git@agrueneberg.info>
2020-04-24 16:58:31 -0400

    Drop hosts alias

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -2,7 +2,6 @@ alias ls='ls --color=auto'
 alias ll='ls -l'
 alias tree='tree -C'
 alias grep='grep --color=auto'
-alias hosts="sudo $EDITOR /etc/hosts"
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias rmarkdown="Rscript -e 'args <- commandArgs(trailingOnly = TRUE); stopifnot(length(args) > 0); rmarkdown::render(args[1])'"
 alias plink='plink --noweb'
