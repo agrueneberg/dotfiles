e86c9e8b55f3efe272d8c4a211eeb6e8d0c85340
Alexander Grueneberg <git@agrueneberg.info>
2021-01-16 12:12:10 -0500

    Drop plink alias

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -4,4 +4,3 @@ alias tree='tree -C'
 alias grep='grep --color=auto'
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias rmarkdown="Rscript -e 'args <- commandArgs(trailingOnly = TRUE); stopifnot(length(args) > 0); rmarkdown::render(args[1])'"
-alias plink='plink --noweb'
