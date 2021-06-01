7d91688e17b8aa794544900a8b0aca5d7da42041
Alexander Grueneberg <git@agrueneberg.info>
2021-06-01 18:06:04 -0400

    Enable ncdu colors

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -1,5 +1,6 @@
 alias ls="ls --color=auto --time-style +'%Y-%m-%d %H:%M'"
 alias tree='tree -C'
+alias ncdu='ncdu --color dark'
 alias grep='grep --color=auto'
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias rmarkdown="Rscript -e 'args <- commandArgs(trailingOnly = TRUE); stopifnot(length(args) > 0); rmarkdown::render(args[1])'"
