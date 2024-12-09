6661766eeed1fe6962507dadb2bb8d1ebbbb3992
Alexander Grueneberg <git@agrueneberg.info>
2024-12-09 12:31:17 +0100

    Add Docker cleanup aliases

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -3,4 +3,6 @@ alias tree='tree -C'
 alias ncdu='ncdu --color dark'
 alias grep='grep --color=auto'
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+alias docker-remove-all-containers="docker container ls -a | tail -n +2 | awk '{print \$1}' | xargs -n 1 docker container rm"
+alias docker-remove-all-images="docker image ls | tail -n +2 | awk '{print \$3}' | xargs -n 1 docker image rm"
 alias rmarkdown="Rscript -e 'args <- commandArgs(trailingOnly = TRUE); stopifnot(length(args) > 0); rmarkdown::render(args[1])'"
