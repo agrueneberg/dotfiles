7952dbda4e94e6e8e202bb783afca7115aea011d
Alexander Grueneberg <git@agrueneberg.info>
2025-01-26 08:55:04 +0100

    Add hx alias

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -2,6 +2,7 @@ alias ls="ls --color=auto --time-style +'%Y-%m-%d %H:%M'"
 alias tree='tree -C'
 alias ncdu='ncdu --color dark'
 alias grep='grep --color=auto'
+alias hx='helix'
 alias git-remove-merged-branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias docker-remove-all-containers="docker container ls -a | tail -n +2 | awk '{print \$1}' | xargs -n 1 docker container rm"
 alias docker-remove-all-images="docker image ls | tail -n +2 | awk '{print \$3}' | xargs -n 1 docker image rm"
