b1bc2f0cde731f34bb3b5e7802bccd730ffab8b0
Alexander Grueneberg <git@agrueneberg.info>
2025-02-27 08:26:32 +0100

    Drop vimplug in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
-git submodule update --init
 mkdir -p ~/.Rlibrary
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
 source ~/.bash_profile
-vim -c 'PlugUpdate' -c 'PlugClean!' -c 'qa' &> /dev/null
